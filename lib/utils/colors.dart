1196924efff182d003774fafa3fbc95c115d308a
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-19 12:47:31 +0330

    Create SafirColors class for custom color definitions
    Added a class for defining custom colors used in the app.

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class SafirColors {
+  // رنگ سبز اختصاصی که در کانوا طراحی کردی
+  static const Color primaryGreen = Color(0xFF145A41);
+  
+  // رنگ‌های کمکی برای هارمونی بیشتر
+  static const Color accentGold = Color(0xFFFDBB2D); // برای مدال‌ها و ستاره‌ها
+  static const Color bgLight = Color(0xFFF8FAFC);   // برای پس‌زمینه صفحات
+  static const Color textDark = Color(0xFF1E293B);  // برای متن‌های اصلی
+}
