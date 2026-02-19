e4a55db4a3f87dd0f98b4c8e4cd04c63b555ba67
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-19 22:18:29 +0330

    Add CargoScreen with input fields and button
    Implemented CargoScreen with text fields and a button for user input.

--- lib/screens/cargo_screen.dart
+++ lib/screens/cargo_screen.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+import '../utils/helper.dart';
+import '../widgets/custom_text_field.dart';
+import '../widgets/custom_button.dart';
 
+class CargoScreen extends StatelessWidget {
+  const CargoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(tr(context, 'cargo_title'))),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            CustomTextField(
+              hintText: tr(context, 'cargo_type'),
+              controller: TextEditingController(),
+              prefixIcon: Icons.local_shipping,
+            ),
+            const SizedBox(height: 15),
+            CustomTextField(
+              hintText: tr(context, 'weight'),
+              controller: TextEditingController(),
+              keyboardType: TextInputType.number,
+              prefixIcon: Icons.line_weight,
+            ),
+            const SizedBox(height: 30),
+            CustomButton(
+              text: tr(context, 'request_ride'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
