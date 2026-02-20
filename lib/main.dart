5a88733b7b3d1b521f9720f453ba15449966d0fd
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-20 17:26:08 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
-import 'app.dart'; // وارد کردن فایل تنظیمات
+import 'package:flutter/services.dart';
+import 'app.dart';
 
-void main() {
-  // اینجا فقط دستور اجرای اپلیکیشن صادر می‌شود
+void main() async {
+  // اطمینان از مقداردهی اولیه ویجت‌ها
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // فیکس کردن جهت صفحه و استایل استاتوس‌بار برای ظاهر مدرن سفیر
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+
   runApp(const SafirApp());
 }
