d586e6854a1899ba508776cb19045b62fadff011
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-20 14:36:42 +0330

    Update custom_text_field.dart

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,34 +1,59 @@
 import 'package:flutter/material.dart';
+import '../utils/helper.dart'; // برای مدیریت زبان سه‌گانه
+import '../utils/colors.dart';
 
 class CustomTextField extends StatelessWidget {
-  final String hintText;
+  final String hintKey; // استفاده از کلید ترجمه به جای متن مستقیم
   final TextEditingController controller;
   final bool obscureText;
   final TextInputType keyboardType;
   final IconData? prefixIcon;
+  final String? Function(String?)? validator; // اضافه شدن قابلیت اعتبارسنجی
+  final int? maxLength;
 
   const CustomTextField({
     Key? key,
-    required this.hintText,
+    required this.hintKey,
     required this.controller,
     this.obscureText = false,
     this.keyboardType = TextInputType.text,
     this.prefixIcon,
+    this.validator,
+    this.maxLength,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: controller,
       obscureText: obscureText,
       keyboardType: keyboardType,
+      validator: validator,
+      maxLength: maxLength,
+      style: const TextStyle(fontFamily: 'IranYekan', fontSize: 15),
       decoration: InputDecoration(
-        hintText: hintText,
-        prefixIcon: prefixIcon != null ? Icon(prefixIcon) : null,
+        hintText: tr(context, hintKey), // ترجمه خودکار متن راهنما
+        prefixIcon: prefixIcon != null ? Icon(prefixIcon, color: SafirColors.primaryGreen) : null,
+        filled: true,
+        fillColor: Colors.grey[100], // پس‌زمینه ملایم برای خوانایی بهتر
+        counterText: "", // مخفی کردن شمارنده حروف در صورت نیاز
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
+          borderRadius: BorderRadius.circular(15), // هماهنگی با شعاع دکمه‌ها
+          borderSide: BorderSide.none, // حذف خط دور در حالت عادی برای ظاهر مدرن‌تر
         ),
-        contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: Colors.grey[300]!, width: 1),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: SafirColors.primaryGreen, width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: const BorderSide(color: Colors.red, width: 1),
+        ),
+        contentPadding: const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
       ),
     );
   }
