be49013c48c20d2c41c96aab01cdc157117c0897
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-20 09:10:58 +0330

    Add new screens and update localization support

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+
+// ایمپورت صفحات
 import 'screens/home_screen.dart';
 import 'screens/home_dashboard.dart';
 import 'screens/fare_test_page.dart';
+import 'screens/driver_reg_screen.dart'; // جدید
+import 'screens/cargo_screen.dart';      // جدید
+import 'screens/intercity_screen.dart';  // جدید
 import 'utils/colors.dart';
 
-// این کلاس همان چیزی است که در main.dart فراخوانی کردیم
 class SafirApp extends StatelessWidget {
   const SafirApp({super.key});
 
@@ -15,21 +19,27 @@ class SafirApp extends StatelessWidget {
       title: 'سفیر | Safir',
       debugShowCheckedModeBanner: false,
       
-      // ۱. تنظیمات زبان افغانستان
+      // ۱. تنظیمات سه‌زبانه (دری، پشتو، انگلیسی)
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [Locale('fa', 'AF')],
-      locale: const Locale('fa', 'AF'),
+      supportedLocales: const [
+        Locale('fa', 'AF'), // دری
+        Locale('ps', 'AF'), // پشتو
+        Locale('en', 'US'), // انگلیسی
+      ],
+      // نکته: این Locale بعداً باید توسط دکمه تغییر زبان عوض شود
+      locale: const Locale('fa', 'AF'), 
 
-      // ۲. تم رنگی پروژه سفیر
+      // ۲. تم رنگی و فونت پروژه سفیر
       theme: ThemeData(
         useMaterial3: true,
         primaryColor: SafirColors.primaryGreen,
         fontFamily: 'IranYekan', 
         scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(seedColor: SafirColors.primaryGreen),
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
           foregroundColor: SafirColors.primaryGreen,
@@ -38,12 +48,15 @@ class SafirApp extends StatelessWidget {
         ),
       ),
 
-      // ۳. لیست تمام صفحات (Routes)
+      // ۳. لیست تمام صفحات (مدیریت مسیرها)
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
         '/dashboard': (context) => const HomeDashboard(),
         '/fare_test': (context) => const FareTestPage(),
+        '/driver_reg': (context) => const DriverRegScreen(), // مسیر جدید
+        '/cargo': (context) => const CargoScreen(),           // مسیر جدید
+        '/intercity': (context) => const IntercityScreen(),   // مسیر جدید
       },
     );
   }
