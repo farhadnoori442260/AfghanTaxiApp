feab22faa9cf6e461ceb1c4045363e62559d3456
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-22 08:30:12 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'dart:async';
 import 'routes.dart';
 
 void main() {
@@ -13,9 +15,63 @@ class SafirApp extends StatelessWidget {
     return MaterialApp(
       title: 'سفیر',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
-      initialRoute: '/',
-      routes: appRoutes,
+      // تنظیمات ۳ زبانه
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa', 'IR'), // فارسی
+        Locale('ps', 'AF'), // پشتو
+        Locale('en', 'US'), // انگلیسی
+      ],
+      locale: const Locale('fa', 'IR'), 
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFF145A41), // رنگ آبی تیره شما
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const MapScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF145A41),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // عکس اسپلش که در Canva ساختی
+            Image.asset('assets/images/splash.png', width: 180),
+            const SizedBox(height: 30),
+            // لودینگ حرکتی سفید رنگ
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
