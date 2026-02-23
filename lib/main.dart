beb73a7a2982ad8df00944414c7b713be8ba082c
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-23 21:41:26 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-// وارد کردن صفحات اصلی از پوشه lib طبق گیت‌هاب شما
 import 'splash_screen.dart';
 import 'home_screen.dart';
-import 'profile_screen.dart';
-import 'profile_edit_screen.dart';
 
 void main() {
   runApp(const SafirApp());
@@ -17,22 +14,15 @@ class SafirApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Safir Taxi',
-      
-      // تنظیم فونت و رنگ‌بندی برند سفیر
       theme: ThemeData(
         primaryColor: const Color(0xFF145A41),
-        fontFamily: 'IranYekan', // فونتی که در pubspec تعریف کردی
+        fontFamily: 'IranYekan', 
         useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white,
       ),
-
-      // مدیریت تمام مسیرهای اپلیکیشن
       initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
         '/home': (context) => const HomeScreen(),
-        '/profile': (context) => const ProfileScreen(), // متصل به فایل profile_screen.dart
-        '/edit_profile': (context) => const ProfileEditScreen(), // متصل به فایل ویرایش
       },
     );
   }
