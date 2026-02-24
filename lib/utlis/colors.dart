bf7e0c11eaee5654de9881981223623f23a8e908
farhadnoori442260 <farhadnoori552@gmail.com>
2026-02-24 05:25:24 +0330

    Create colors.dart

---
+++ lib/utlis/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SafirColors {
+  static const Color primaryGreen = Color(0xFF145A41);
+  static const Color accentGold = Color(0xFFD4AF37);
+  static const Color errorRed = Color(0xFFD32F2F);
+  static const Color backgroundLight = Color(0xFFF8F9FA);
+  static const Color darkCard = Color(0xFF1E1E1E);
+  static const Color textGrey = Color(0xFF757575);
+}
